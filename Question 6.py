d2888d4faf96ca431e8174f19a47c0ade53050b9
Kasım Çarbuga <58843807+kasimcarbuga@users.noreply.github.com>
2023-03-09 23:02:31 +0300

    Create Question 6.py

---
+++ Question 6.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('C:/Users/kasim/Desktop/BELGELER/country_vaccination_stats.csv')
+
+print(data.isnull().any())
+
+countries = data['country'].unique()
+
+for c in countries:
+    
+    min_vac = data[data['country']==c].daily_vaccinations.min()
+    
+    if np.isnan(min_vac):
+        
+        values = {'daily_vaccinations': 0}
+        
+    else:
+        
+
+        values = {'daily_vaccinations': min_vac}
+        
+    data[data['country'] == c] = data[data['country'] == c].fillna(value=values)
+
+
+
+print(data.isnull().any())
+
+
+
+
+median = data.groupby("country")["daily_vaccinations"].median()
+
+sorting= median.sort_values(ascending=False)
+
+print(sorting.head(3))
